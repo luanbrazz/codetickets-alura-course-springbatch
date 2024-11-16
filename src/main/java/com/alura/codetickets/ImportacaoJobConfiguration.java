c4cd9c4af9ad1c2bec967e840375e2ce1fa2127f
Luan Braz <luan_silvacpv@hotmail.com>
2024-11-16 17:11:56 -0300

    FIXING ERRORS: - Caused by: java.lang.IllegalArgumentException: dataSource or dataSourceClassName or jdbcUrl is required.
    - Caused by: org.springframework.batch.item.file.transform.IncorrectTokenCountException: Incorrect number of tokens found in record: expected 7 actual 1
    
    -Error when converting String values to java.time.LocalDate in the birth and date fields, due to the lack of a suitable converter or editor.
    
    - Defining a fieldSetMapper

--- src/main/java/com/alura/codetickets/ImportacaoJobConfiguration.java
+++ src/main/java/com/alura/codetickets/ImportacaoJobConfiguration.java
@@ -52,6 +52,7 @@ public class ImportacaoJobConfiguration {
                 .resource(new FileSystemResource("files/dados.csv"))
                 .comments("--")
                 .delimited()
+                .delimiter(";")
                 .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
                 .targetType(Importacao.class)
                 .build();
