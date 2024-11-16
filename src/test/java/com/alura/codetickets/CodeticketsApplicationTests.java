7d3600c7320433fb4454a08f4a53acd7171b1520
Luan Braz <luan_silvacpv@hotmail.com>
2024-11-15 23:53:56 -0300

    started spring batch project and import entity

---
+++ src/test/java/com/alura/codetickets/CodeticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.codetickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
